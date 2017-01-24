b1de1127bb322d4425be406f774321c6171ff94c
ohmyginger <ohmyginger1994@gmail.com>
2017-01-23 19:38:12 -0600

    library progress

--- sources/cstdlib/stdio.h
+++ sources/cstdlib/stdio.h
@@ -1,8 +1,32 @@
 #ifndef __STDIO_H__
 #define __STDIO_H__
 
+#include "unistd.h"
 
 #define EOF ((int)-1)
+
+
+int putchar(int ch)
+{
+  if ( syscall(SC_PUT_CHAR, (unsigned char)ch) == 0 )
+    return ch;
+  else
+    return EOF;
+}
+
+
+int puts(const char *str)
+{
+  if ( syscall(SC_PRINT_STRING, str) != -1 )
+  {
+    if ( putchar('\n') != EOF )
+      return 1;
+    else
+      return EOF;
+  }
+  else
+    return EOF;
+}
 
 
 #endif
